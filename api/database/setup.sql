ac03913436e1e390a58d17a51040c3a6365f15c5
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-21 15:23:21 +0100

    created tables in dtb

---
+++ api/database/setup.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS users, pokemon, users_pokemon;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    hashed_password VARCHAR NOT NULL,
+    profile_image_url VARCHAR,
+    keys INT,
+    -- pomodoro settings
+    block_num INT,
+    block_mins INT,
+    short_break_mins INT,
+    long_break_mins INT
+);
+
+CREATE TABLE pokemon (
+    pokemon_id SERIAL PRIMARY KEY,
+    pokemon_name VARCHAR,
+    front_image_url VARCHAR,
+    back_image_url VARCHAR,
+    powers VARCHAR,
+    egg_image_url VARCHAR,
+    keysNeeded INT
+);
+
+CREATE TABLE users_pokemon (
+    users_pokemon_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) NOT NULL,
+    pokemon_id INT REFERENCES pokemon(pokemon_id) NOT NULL
+);

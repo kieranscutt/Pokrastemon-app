d5c2a735847e5ec85b00a168fc039b353173f518
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-25 11:30:50 +0100

    changed pokemon images to gifs

--- api/database/fetchPokemon.js
+++ api/database/fetchPokemon.js
@@ -6,10 +6,12 @@ const fetchPokemon = async() => {
             const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             const pokemon = await resp.json()
             const { name,sprites,moves,types,id } = pokemon
-            const { back_default, front_default } = sprites
+            const { versions } = sprites
+            const back_default = versions["generation-v"]["black-white"].animated.back_default
+            const front_default = versions["generation-v"]["black-white"].animated.front_default
             const typeNames = types.map((t) => t.type.name)
             const moveNames = moves.map((m) => m.move.name)
-            await db.query("INSERT INTO pokemon(pokemon_id,pokemon_name,pokedex_number,front_image_url,back_image_url,moves,types) VALUES ($1,$2,$3,$4,$5,$6,$7)",[id,name,id,front_default,back_default,moveNames,typeNames])
+            await db.query("INSERT INTO pokemon(pokemon_id,pokemon_name,front_image_url,back_image_url,moves,types) VALUES ($1,$2,$3,$4,$5,$6)",[id,name,front_default,back_default,moveNames,typeNames])
         } catch (err) {
             console.log(err)
             return err

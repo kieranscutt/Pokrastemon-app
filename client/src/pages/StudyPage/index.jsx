3d258f4731d58be3ac7bb93db160ef473702d284
kieranscutt <kieranscutt142@gmail.com>
2023-07-25 15:58:38 +0100

    Bugs

--- client/src/pages/StudyPage/index.jsx
+++ client/src/pages/StudyPage/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
+import { Timer, LootBox } from '../../components';
 
 const StudyPage = () => {
+  const [keys, setKeys] = useState(0)
+  const [chest, setChest] = useState('closed')
+
+
+  
   return (
     <div>
       <h1>Study Page</h1>
+      <Timer />
+      <LootBox />
     </div>
   );
 };

984bb99eceb3f4a4929115ad3d38b28edac74d49
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-25 16:59:31 +0100

    fixed keys display

--- client/src/components/nav/index.jsx
+++ client/src/components/nav/index.jsx
@@ -34,15 +34,11 @@ const NavBar = () => {
         setKeyCount(data.keys)
       })
     }
-  },[])
+  },[isLoggedIn])
 
-  const showKeys = () => {
-    const key = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></svg>'
-    return (<div dangerouslySetInnerHTML={{ __html: key.repeat(keyCount)}}></div>)
-  }
+  const key = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></svg>'
 
   const logout = async () => {
-    console.log('logging out')
     const token = localStorage.getItem("token");
     const options = {
       method: "DELETE",
@@ -53,7 +49,6 @@ const NavBar = () => {
     const resp = await fetch('https://pokrastemon-api.onrender.com/users/logout', options)
     const data = await resp.json()
     if (resp.ok) {
-      console.log(data)
       localStorage.removeItem('token')
       window.location.href = '/login'
     } else {
@@ -65,7 +60,10 @@ const NavBar = () => {
     <Navbar expand="lg" className="navbar-container">
       <Container fluid>
         <Nav>
-          <div className="nav-keys">{showKeys()}</div> {/* Add the keys count */}
+          <div className="nav-keys"
+          dangerouslySetInnerHTML={{ __html: key.repeat(keyCount)}}
+          >
+          </div>
         </Nav>
         <Navbar.Brand href="/">Pokrastemon Adventures</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />

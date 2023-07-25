eb8e1d42e44ab5693451a6861142a56b8ae9dbd2
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-25 16:51:59 +0100

    logout function

--- client/src/components/nav/index.jsx
+++ client/src/components/nav/index.jsx
@@ -15,35 +15,57 @@ const NavBar = () => {
   ];
 
   const [keyCount,setKeyCount] = useState(0)
-  const isLoggedIn = localStorage.getItem('token') ? true : false
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    const options = {
-      method: "GET",
-      headers: {
-        authorization: token,
-      },
+    setIsLoggedIn(localStorage.getItem('token') ? true : false)
+    if (isLoggedIn) {
+      const token = localStorage.getItem("token");
+      const options = {
+        method: "GET",
+        headers: {
+          authorization: token,
+        },
+      }
+      fetch('https://pokrastemon-api.onrender.com/users/user', options)
+      .then(resp => resp.json())
+      .then(data => {
+        setKeyCount(data.keys)
+      })
     }
-    fetch('https://pokrastemon-api.onrender.com/users/user', options)
-    .then(resp => resp.json())
-    .then(data => {
-      setKeyCount(data.keys)
-    })
   },[])
 
   const showKeys = () => {
     const key = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></svg>'
     return (<div dangerouslySetInnerHTML={{ __html: key.repeat(keyCount)}}></div>)
-    }
+  }
 
-  const keys = showKeys()
+  const logout = async () => {
+    console.log('logging out')
+    const token = localStorage.getItem("token");
+    const options = {
+      method: "DELETE",
+      headers: {
+        authorization: token,
+      },
+    }
+    const resp = await fetch('https://pokrastemon-api.onrender.com/users/logout', options)
+    const data = await resp.json()
+    if (resp.ok) {
+      console.log(data)
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    } else {
+      console.log(data)
+    }
+  }
 
   return (
     <Navbar expand="lg" className="navbar-container">
       <Container fluid>
         <Nav>
-          <div className="nav-keys">{keys}</div> {/* Add the keys count */}
+          <div className="nav-keys">{showKeys()}</div> {/* Add the keys count */}
         </Nav>
         <Navbar.Brand href="/">Pokrastemon Adventures</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
@@ -57,7 +79,7 @@ const NavBar = () => {
           </Nav>
           <Nav>
             {isLoggedIn ? (
-              <Nav.Link href="/logout" className={location.pathname === '/logout' ? 'active' : ''}>
+              <Nav.Link className={location.pathname === '/logout' ? 'active' : ''} onClick={() => logout()}>
                 Log Out
               </Nav.Link>
             ) : (

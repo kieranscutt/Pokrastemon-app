5def56f55ffcc5ed1e6af63cfe8c22c082de2781
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2023-07-26 15:04:56 +0100

    Library page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import PageWrapper from './components/pageWrapper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/Nav/styles.css';
 import { HomePage, LibraryPage, ProfilePage, StudyPage, LoginPage } from './pages';
-// import './App.css';
 import NavBar from './components/Nav';
 
 const App = () => {
+    const collectedPokemons = [
+    // { id: 25, name: 'Pikachu', description: 'An electric-type Pokémon.' },
+    // { id: 4, name: 'Charmander', description: 'A fire-type Pokémon.' },
+  ];
   return (
     <>
         <NavBar/>
@@ -17,9 +20,8 @@ const App = () => {
             <Route path="/login" element={<LoginPage />} />
             <Route path='/study' element={<StudyPage />} />
             <Route path='/profile' element={<ProfilePage />} />
-            <Route path='/library' element={<LibraryPage />} />
-            <Route path='/library/:id' element={<LibraryPage />} />
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
+            <Route path='/library/:id'  element={<LibraryPage collectedPokemons={collectedPokemons} />} />
+            <Route path='/library' element={<LibraryPage collectedPokemons={collectedPokemons} />} />            {/* <Route path="*" element={<NotFoundPage />} /> */}
           </Routes>
         </PageWrapper>
     </>
--- client/src/pages/LibraryPage/index.jsx
+++ client/src/pages/LibraryPage/index.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 
 const LibraryPage = ({ collectedPokemons }) => {
-  if (!collectedPokemons || !Array.isArray(collectedPokemons) || collectedPokemons.length === 0) {
-    return (
-      <div>
-        <h1>My Pokémon Library</h1>
-        <p>Welcome, Trainer! You haven't collected any Pokémon yet. Get out there and catch 'em all by studying!</p>
-      </div>
-    );
-  }
-
   return (
     <div>
-      <h1>My Pokémon Library</h1>
-      <p>Welcome, Trainer! Below is a list of Pokémon you have collected on your journey. Gotta catch 'em all!</p>
-      <div className="pokemon-collection">
-        {collectedPokemons.map((pokemon) => (
-          <div key={pokemon.id} className="pokemon-card">
-            <h2>{pokemon.name}</h2>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
-            <p>{pokemon.description}</p>
+      <h1>Welcome to Your Pokémon Library</h1>
+      {collectedPokemons.length === 0 ? (
+        <p>You haven't collected any Pokémon yet. Get out there and catch 'em all by opening chests from studying!</p>
+      ) : (
+        <>
+          <p>Below is a list of Pokémon you have collected on your journey. Gotta catch 'em all all by opening chests from studying!</p>
+          <div className="pokemon-collection">
+            {collectedPokemons.map((pokemon) => (
+              <div key={pokemon.id} className="pokemon-card">
+                <h2>{pokemon.name}</h2>
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+                <p>{pokemon.description}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 };

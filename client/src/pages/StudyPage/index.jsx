a023332e732d9c5249a7bbbf2aeb32638388ac30
kieranscutt <kieranscutt142@gmail.com>
2023-07-26 14:11:01 +0100

    Merging

--- client/src/pages/StudyPage/index.jsx
+++ client/src/pages/StudyPage/index.jsx
@@ -5,17 +5,12 @@ import SettingsModal from '../../components/SettingsModal';
 import './styles.css'
 
 const StudyPage = () => {
-<<<<<<< HEAD
- const [keys, setKeys] = useState(0);
- const [chest, setChest] = useState("closed")
-=======
 
   const [modalStatus, setModalStatus] = useState(false)
  
   const showModal = () => {
     setModalStatus(true)
   }
->>>>>>> bf4e7f7c1366d084e10450f794adaf403dfe3f40
 
   const hideModal = () => {
     setModalStatus(false)

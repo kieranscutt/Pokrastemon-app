75323c00adf6b014fa160a8b477369283add1853
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2023-07-26 16:31:38 +0100

    Added pokemon to library

--- client/src/pages/LibraryPage/index.jsx
+++ client/src/pages/LibraryPage/index.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import './styles.css';
 
 const LibraryPage = ({ collectedPokemons }) => {
   return (
-    <div>
+    <div className='pokemon-library'>
       <h1>Welcome to Your Pokémon Library</h1>
-      {collectedPokemons.length === 0 ? (
-        <p>You haven't collected any Pokémon yet. Get out there and catch 'em all by opening chests from studying!</p>
-      ) : (
-        <>
-          <p>Below is a list of Pokémon you have collected on your journey. Gotta catch 'em all all by opening chests from studying!</p>
-          <div className="pokemon-collection">
-            {collectedPokemons.map((pokemon) => (
-              <div key={pokemon.id} className="pokemon-card">
-                <h2>{pokemon.name}</h2>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
-                <p>{pokemon.description}</p>
-              </div>
-            ))}
+      <div className='pokemon-grid'>
+        <p>
+          {collectedPokemons.length === 0 ? (
+            "You haven't collected any Pokémon yet. Get out there and catch 'em all by opening chests from studying!"
+          ) : (
+            "Below is a list of Pokémon you have collected on your journey. Gotta catch 'em all by opening chests from studying!"
+          )}
+        </p>
+      </div>
+      <div className='pokemon-card-container'>
+        {collectedPokemons.map((pokemon) => (
+          <div key={pokemon.id} className="pokemon-card">
+            <h2>{pokemon.name}</h2>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+            <p>{pokemon.description}</p>
           </div>
-        </>
-      )}
+        ))}
+      </div>
     </div>
   );
 };

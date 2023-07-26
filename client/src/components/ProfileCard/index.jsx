ff2002babc5549a4f3f1ca63c31f2ae4066984a3
kieranscutt <kieranscutt142@gmail.com>
2023-07-26 12:50:29 +0100

    Profile card

---
+++ client/src/components/ProfileCard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProfileCard({ profile }) {
+    return (
+        <>
+        <h1>{profile.username}</h1>
+        <h3>Number of pokemon:</h3>
+        <h3>Keys:</h3>
+        </>
+    )
+}
--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router';
+import { ProfileCard } from '../../components';
+
 
 const ProfilePage = () => {
+const {id} = useParams();
+const [profile, setProfile] = useState([]);
+
+
+useEffect(() => {
+
+  async function displayUser() {
+    const resp = await fetch(`https://pokrastemon-api.onrender.com/users/${id}`);
+    const data = await resp.json();
+    setProfile(data)
+}
+
+displayUser();
+
+}, []);
+
   return (
     <div>
-      <h1>Profile Page</h1>
+      <ProfileCard profile={profile}/>
     </div>
   );
 };

f151454aae163d22f774029d7db7748006958428
kieranscutt <kieranscutt142@gmail.com>
2023-07-27 17:06:20 +0100

    Deleted Profile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/nav/styles.css';
-import { HomePage, LibraryPage, ProfilePage, StudyPage, LoginPage } from './pages';
+import './App.css';
+import { HomePage, LibraryPage, StudyPage, LoginPage } from './pages';
 import NavBar from './components/nav';
 
 const App = () => {
@@ -37,7 +37,6 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/study" element={<StudyPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
         <Route
           path="/library"
           element={<LibraryPage collectedPokemons={collectedPokemons} isLoggedIn={isLoggedIn} />}
